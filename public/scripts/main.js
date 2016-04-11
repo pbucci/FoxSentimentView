45ceb8c2d88c4210982131b04e0a2d2d15bd3bb8
pbucci <pbucci@gmail.com>
2016-04-11 09:41:32 -0700

    to the point of displaying fake sentiment data

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -59,7 +59,6 @@ var Words = React.createClass({
     });
     var end = {text:text.slice(s,e),concept:"none",sentiment:"none"}
     out.push(end)
-    console.log(out)
     
     var spacer = function(text) {
         return text.map(function(t){
@@ -127,8 +126,91 @@ var Story = React.createClass({
   }
 });
 
+
+class CountMap {
+  constructor() {
+    this.map = new Map()
+  }
+  add(i,n) {
+    if (this.map.has(i)) {
+      this.map.set(i,this.map.get(i) + n)
+    }
+    else {
+      this.map.set(i,n)
+    }
+    return [i,this.map.get(i)]
+  }
+  get(i) {
+    return this.map.get(i)
+  }
+  keys() {
+    return this.map.keys()
+  }
+  keyvals() {
+    return Array.from(this.map.entries());
+  }
+};
+
+var Bar = React.createClass({
+  render: function() {
+    var sentimap = {
+      "+":"positive",
+      "-":"negative",
+      "0":"neutral"
+    }
+    return (<div className={sentimap[this.props.data]}></div>)
+  }
+})
+
+var BarGraph = React.createClass({
+  render: function() {
+    var concept = this.props.concept
+    var count = 0
+    var barmap = this.props.data.split("").map(function(s){
+      count++//needed for unique keys
+      return(<Bar key={concept+count} data={s} />)
+    })
+    return (<div className="graph">{barmap}</div>)
+  }
+});
+
+var Visualizer = React.createClass({
+  render: function() {
+    
+      var concept_set = new CountMap()
+      var concept_vis = new CountMap()
+      this.props.data['paragraphs'].forEach(function(p){
+        p['concepts'].forEach(function(c) {
+          concept_set.add(c['label'],1)
+          console.log(concept_vis.add(c['label'],c['s']))
+        });
+      });
+
+    var pairs = concept_set.keyvals().map(function([k,v]){
+      var vis = concept_vis.get(k)
+      console.log(vis)
+      return(<div key={k} className="concept">
+              <div className="concept_text">
+                <strong>{k}</strong> : {v}
+              </div>
+              <BarGraph concept={k} data={vis} />
+             </div>
+      )
+    })
+
+    return (
+      <div className="sentiments">
+        {pairs}
+      </div>
+    )
+  },
+});
+
 // Main function
 ReactDOM.render(
-  <Story data={data}/>,
+  <div>
+    <Visualizer data={data}/>
+    <Story data={data}/>
+  </div>,
   document.getElementById('content')
 );
